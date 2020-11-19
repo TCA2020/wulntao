130eb8474295bd2fdb30516684c73b70ce539dbb
tcagame <tcagame@tca.ac.jp>
2020-11-19 18:49:01 +0900

    宿題提出です
    前期提出したプログラムをシリアライズを使って遊んだブレやを記録しました。

---
+++ 宿题提出/ConsoleApp1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//Serializable;
+namespace ConsoleApp1
+{
+    class shi
+    {
+        public shi() { }
+        public shi(string name)
+        {
+            this.name = name;
+        }
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+       public void SayHi()
+        {
+            Console.WriteLine("プレイヤ{0}胜利!!!", name);
+        }
+    }
+}
